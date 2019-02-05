2eea18cf6961fac345e45fe50e5b48491da5e94f
viktinho56 <viktinho56@gmail.com>
2019-02-05 11:30:19 +0100

    Add files via upload
    cool

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var storeViewModel = require("./viewmodel/storeViewModel");
+var bankViewModel = require("./viewmodel/bankViewModel");
+var productsViewModel = require("./viewmodel/productsViewModel");
+var imageViewModel = require("./viewmodel/imageViewModel");
+var  app = express();
+//set up template engine
+app.set("view engine","ejs");
+
+//static files
+app.use(express.static('./public'));
+
+//fire viewmodel
+app.get("/",function(req,res){
+    res.render(__dirname+"/views/todo");
+
+});
+storeViewModel(app);
+bankViewModel(app);
+productsViewModel(app);
+imageViewModel(app);
+
+//set port
+app.listen(5000,function(){
+console.log("now listening on port 5000");
+});
